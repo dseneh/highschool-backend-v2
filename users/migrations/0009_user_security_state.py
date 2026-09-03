322a78c9382790fe8ad065e1ac4bb3261f92a384
dseneh <54472523+dseneh@users.noreply.github.com>
2026-09-02 22:26:31 -0500

    Limit security state migration to token revocation

--- users/migrations/0009_user_security_state.py
+++ users/migrations/0009_user_security_state.py
@@ -10,29 +10,4 @@ class Migration(migrations.Migration):
             name="security_version",
             field=models.PositiveBigIntegerField(default=1),
         ),
-        migrations.AddField(
-            model_name="user",
-            name="mfa_enabled",
-            field=models.BooleanField(default=False),
-        ),
-        migrations.AddField(
-            model_name="user",
-            name="mfa_required",
-            field=models.BooleanField(default=False),
-        ),
-        migrations.AddField(
-            model_name="user",
-            name="mfa_secret_envelope",
-            field=models.JSONField(blank=True, editable=False, null=True),
-        ),
-        migrations.AddField(
-            model_name="user",
-            name="mfa_confirmed_at",
-            field=models.DateTimeField(blank=True, null=True),
-        ),
-        migrations.AddField(
-            model_name="user",
-            name="mfa_recovery_code_hashes",
-            field=models.JSONField(blank=True, default=list, editable=False),
-        ),
     ]
